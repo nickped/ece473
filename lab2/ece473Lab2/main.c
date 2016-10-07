ad4259b612a0884d44d5aa2ceb6d99e682bc5105
Nick <nick.a.ped@gmail.com>
2016-10-07 14:18:12 -0700

    Final commit

--- lab2/ece473Lab2/main.c
+++ lab2/ece473Lab2/main.c
@@ -2,7 +2,10 @@
  * main.c
  *
  *  Created on: Sep 29, 2016
- *      Author: Nick
+ *      Author: Nick Pederson
+ *         MCU:	ATMEGA128
+ *     Purpose: This code reads the pushbutton board and displays the running sum on the 7 segment board
+ *     			The buttons are read as B0 = 1, B1 = 2, B2 = 4, ..., B7 = 128
  */
 #include<avr/io.h>
 #ifndef F_CPU
@@ -10,34 +13,41 @@
 #endif
 #include<util/delay.h>
 
-#define LED_ON 1 // loops this many time when LED is on
-#define DEBOUNCE_TIME 6 // Number of times it read until it acknowledge
+#define LED_ON 1 // loops this many time through 7 seg
+#define DEBOUNCE_TIME 6 // Number of button reads until it acknowledge button press
 
-static uint16_t LED_num = 0;
-static uint8_t currentButton = 0,
-			   debounceCnt = 0;
+static uint16_t LED_num = 0; // Current sum
+static uint8_t lastButton = 0, // The buttons press from the last check
+			   debounceCnt = 0; // Current count of button checks without changing lastButton
 
+/* Input:
+ * output: The debounced value of the push buttons
+ * Purpose: Reads and debounces the push buttons
+ */
 uint8_t readButtons(){
 	uint8_t result;
-	DDRA = 0x00; //1.5 clk to switch ddr
-	_delay_us(2); // delays 32 clk cycles
+	DDRA = 0x00; // Set port A for input
 
 	PORTB = 0xF0; // Enables buttons and disable 7 seg
 	PORTA = 0xFF; // Enables all push buttons with pull up resister
 	result = PINA ^ 0xFF;
 	_delay_us(2); // Delay for reading PIN
 
-	//if buttons change reset currentButtons and debounceCnt
-	if(result != currentButton){
-		currentButton = result;
+	//if button change, reset lastButton and debounceCnt
+	if(result != lastButton){
+		lastButton = result;
 		debounceCnt = 0;
 		return 0x00;
 	}
-	if(result == 0x00) return 0x00; // returns no pushed buttons
-	if(result == currentButton && debounceCnt <= DEBOUNCE_TIME) debounceCnt++; //if the result equals the previous pushed button it iterates the debounceCnt without risk of rolling over
+	//if(result == 0x00) return 0x00; // returns no pushed buttons
+	if(result == lastButton && debounceCnt <= DEBOUNCE_TIME) debounceCnt++; //if the result equals the previous pushed button it iterates the debounceCnt without risk of rolling over
 	return (debounceCnt == DEBOUNCE_TIME) ? result : 0x00; // returns result if debounce is true otherwise returns no pushed Buttons
 }
 
+/* Input: decimal value to be converted. Must be a single digit
+ * Output: Returns the 7seg representation of the input
+ * Purpose: converts a decimal digit to 7seg display
+ */
 uint8_t dec2bin(uint8_t dec){
 	switch(dec){
 		case 0: return 0x3F;
@@ -54,51 +64,46 @@ uint8_t dec2bin(uint8_t dec){
 	return 0;
 }
 
+/* Input:
+ * Output:
+ * Purpose: Displays LED_num to 7seg
+ */
 void write7seg(){
-	DDRA = 0xFF; //Needs 1.5 clk to switch
+	DDRA = 0xFF; //Sets port A to output
 	uint8_t dig1,
 		    dig2,
 			dig3,
 			dig4;
 
-	dig1 = dec2bin(LED_num % 10    );
-	dig2 = dec2bin(LED_num % 100   / 10);
-	dig3 = dec2bin(LED_num % 1000  / 100);
-	dig4 = dec2bin(LED_num % 10000 / 1000);
+	dig1 = dec2bin(LED_num % 10    );		// Calculates the 7seg number for the first digit
+	dig2 = dec2bin(LED_num % 100   / 10);	// Calculates the 7seg number for the second digit
+	dig3 = dec2bin(LED_num % 1000  / 100);	// Calculates the 7seg number for the third digit
+	dig4 = dec2bin(LED_num % 10000 / 1000);	// Calculates the 7seg number for the fourth digit
 
 	for(uint8_t i = 0; i < LED_ON; i++){
-		PORTA = 0xFF;
+		PORTB = 0x00; 			// Enables the first digit
+		PORTA = dig1 ^ 0xFF; 	// writes the first digit
+		_delay_ms(2);			// Deghosting delay
 
-		PORTB = 0x00; // Enables the first digit
-		PORTA = dig1 ^ 0xFF; // writes the first digit
-		_delay_ms(2);
-		//PORTA = 0xFF;
+		PORTB = 0x10;			// Enables the second digit
+		PORTA = dig2 ^ 0xFF;	// writes the second digit
+		_delay_ms(2);			// Deghosting delay
 
-		PORTB = 0x10;
-		PORTA = dig2 ^ 0xFF;
-		_delay_ms(2);
-		//PORTA = 0xFF;
+		PORTB = 0x30;			// Enables the third digit
+		PORTA = dig3 ^ 0xFF;	// writes the third digit
+		_delay_ms(2);			// Deghosting delay
 
-		PORTB = 0x30;
-		PORTA = dig3 ^ 0xFF;
-		_delay_ms(2);
-		//PORTA = 0xFF;
-
-		PORTB = 0x40;
-		PORTA = dig4 ^ 0xFF;
-		_delay_ms(2);
-		//PORTA = 0xFF;
+		PORTB = 0x40;			// Enables the fourth digit
+		PORTA = dig4 ^ 0xFF;	// writes the fourth digit
+		_delay_ms(2);			// Deghosting delay
 	}
 }
 
 int main(){
-	DDRB = 0xF0;
-	LED_num = 0;
+	DDRB = 0xF0; // Sets upper 4 bits of port B to output for decoder
+	LED_num = 0; // Sets current number displayed as 0
 	while(1){
-		write7seg();
-		//LED_num++;
-		LED_num += readButtons();
-		//if(LED_num > 9999) LED_num = 0;
-		//_delay_ms(2);
+		write7seg(); // Writes current number to 7seg
+		LED_num += readButtons(); //Read and add pushbuttons to current number
 	}
 }
