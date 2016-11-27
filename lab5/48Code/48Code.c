05b6751007c3360601b3f775260fcfd47b634a57
Nick <nick@localhost.localdomain>
2016-11-27 14:14:58 -0800

    working atmega 48 code

---
+++ lab5/48Code/48Code.c
@@ -0,0 +1,42 @@
+#include<avr/io.h>
+#include<avr/interrupt.h>
+#include<util/twi.h>
+#include<util/delay.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include"twi_master.h"
+#include"48Code.h"
+#include"uart_functions_m48.h"
+
+#define SEND_FLAG 0x10
+
+uint8_t celsius[2];
+uint8_t readTwiBuffer[2];
+char strBuffer[10];
+uint8_t flag = 0x00;
+
+int main(){
+   DDRD |= 0x10; //for testing
+   init_twi();
+   uart_init();
+
+   celsius[0] = 0x00;
+   celsius[1] = 0x00;
+   twi_start_wr(SENSOR_ADDR, celsius, 2); //sets up registers on temp sensor
+
+   sei();
+   while(1){
+      if(UCSR0A & (1<<RXC0)){
+	 PORTD |= 0x10;
+	 celsius[0] = UDR0;
+	 flag &= ~SEND_FLAG;
+	 twi_start_rd(SENSOR_ADDR, readTwiBuffer, 2);
+	 readTwiBuffer[0] = (readTwiBuffer[0]<<1) | ((readTwiBuffer[1]&0x80) ? 1 : 0);
+
+	 sprintf(strBuffer, "%d.%d\n", readTwiBuffer[0], ((readTwiBuffer[1] & 0x40) ? 50 : 0) + ((readTwiBuffer[1] & 0x20) ? 25 : 0)) ;
+	 uart_puts(strBuffer);
+
+	 PORTD &= ~0x10;
+      }
+   }
+}
---
+++ lab5/48Code/48Code.h
@@ -0,0 +1,8 @@
+#ifndef _48CODE
+#define _48CODE
+
+#define SENSOR_ADDR 0x90
+
+#endif
+//void uartPutc(uint8_t data);
+//void uartInit();
