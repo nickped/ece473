411e0db5c260188242ae3af6b510b15a66070abc
nickped <pederson@oregonstate.edu>
2016-12-01 05:30:30 +0000

    comments added

--- lab5/48Code/48Code.c
+++ lab5/48Code/48Code.c
@@ -8,35 +8,33 @@
 #include"48Code.h"
 #include"uart_functions_m48.h"
 
-#define SEND_FLAG 0x10
-
 uint8_t celsius[2];
-uint8_t readTwiBuffer[2];
-char strBuffer[10];
-uint8_t flag = 0x00;
+uint8_t readTwiBuffer[2]; //twi read buffer
+char strBuffer[10]; //uart send buffer
 
 int main(){
    DDRD |= 0x10; //for testing
-   init_twi();
-   uart_init();
+   init_twi(); //initialize twi
+   uart_init(); //initialize uart
 
-   celsius[0] = 0x00;
+   celsius[0] = 0x00; //load pointer register
    celsius[1] = 0x00;
    twi_start_wr(SENSOR_ADDR, celsius, 2); //sets up registers on temp sensor
 
-   sei();
+   sei(); //used for twi
    while(1){
+      //checks uart receive complete flag
       if(UCSR0A & (1<<RXC0)){
-	 PORTD |= 0x10;
-	 celsius[0] = UDR0;
-	 flag &= ~SEND_FLAG;
-	 twi_start_rd(SENSOR_ADDR, readTwiBuffer, 2);
-	 readTwiBuffer[0] = (readTwiBuffer[0]<<1) | ((readTwiBuffer[1]&0x80) ? 1 : 0);
+	 PORTD |= 0x10; //turn on led
+	 celsius[0] = UDR0; //clear receive complete flag
+	 twi_start_rd(SENSOR_ADDR, readTwiBuffer, 2); //read twi
+	 readTwiBuffer[0] = (readTwiBuffer[0]<<1) | ((readTwiBuffer[1]&0x80) ? 1 : 0); //shifts upper byte left and loads msb of lower byte
 
+	 //creates string to send. Always prints 2 digits after decimal
 	 sprintf(strBuffer, "%d.%02d", readTwiBuffer[0], ((readTwiBuffer[1] & 0x40) ? 50 : 0) + ((readTwiBuffer[1] & 0x20) ? 25 : 0)) ;
-	 uart_puts(strBuffer);
+	 uart_puts(strBuffer); //sent to uart
 
-	 PORTD &= ~0x10;
+	 PORTD &= ~0x10; //turn off led
       }
    }
 }
--- lab5/48Code/48Code.h
+++ lab5/48Code/48Code.h
@@ -4,5 +4,3 @@
 #define SENSOR_ADDR 0x90
 
 #endif
-//void uartPutc(uint8_t data);
-//void uartInit();
