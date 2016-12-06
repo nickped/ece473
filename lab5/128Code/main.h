ee1c7833224fa1a8bc76c16bec8519c07a5af6bb
Nick <nick@localhost.localdomain>
2016-12-05 22:39:29 -0800

    Working lab5git add ./* ../48Code/*

---
+++ lab5/128Code/main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN
+#define _MAIN
+
+
+void timerInit();
+uint8_t adcOffset(uint8_t adcVal);
+void checkAlarm();
+void alarmSet();
+void setLcdString();
+void iterateTime(int8_t hr, int8_t min, int8_t sec);
+void iterateAlarm(int8_t hr, int8_t min, int8_t sec);
+void adcInit();
+int readButton();
+void changeTime(uint8_t buttons, uint8_t clock);
+void spiInit();
+uint8_t dec2seg(uint8_t dec);
+void write7Seg(uint8_t hr, uint8_t min, uint8_t sec);
+
+#endif
